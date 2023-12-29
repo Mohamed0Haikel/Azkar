997eb83f73fb156ede2af67ed5dea02701aad284
= <=>
2023-12-29 23:42:21 +0200

    Azkar -V QScreen

---
+++ lib/modules/mushaf.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+class QScreen extends StatelessWidget {
+  const QScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
